e508adc167227d35204cec5a2befefa7e3459a7c
Sergeileduc <sergei.leduc@gmail.com>
2020-02-16 22:56:37 +0100

    Bump version: 0.1.2 → 0.1.3

--- py_casim/__init__.py
+++ py_casim/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Sergeileduc"""
 __email__ = 'sergei.leduc@gmail.com'
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 from .casim import Casim  # noqa: F401
--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    version='0.1.2',
+    version='0.1.3',
     zip_safe=False,
 )
