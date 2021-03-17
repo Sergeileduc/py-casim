1ae9d08c26398e8180132b4a4f2d1f82c1970b6c
Sergeileduc <sergei.leduc@gmail.com>
2021-03-17 14:18:44 +0100

    More tests

--- tests/test_casim_logged.py
+++ tests/test_casim_logged.py
@@ -6,7 +6,7 @@ import re
 
 import pytest
 
-from py_casim.casim import Casim
+from py_casim.casim import CasimLogged
 
 
 @pytest.fixture(scope='function')
@@ -16,7 +16,7 @@ def casim_inst(shared_datadir):
     See more at: http://doc.pytest.org/en/latest/fixture.html
     """
     image = (shared_datadir / 'sample image.png')
-    return Casim(image, resize=125)
+    return CasimLogged(image, resize=125)
 
 
 def test_repr(casim_inst):
@@ -27,14 +27,16 @@ def test_repr(casim_inst):
 
 def test_get_link(casim_inst):
     """Test Casim.get_link()."""
+    casim_inst.change_folder("Scantrad")
     res = casim_inst.get_link()
-    expected = "https://nsa40.casimages.com/img/2020/02/17/200217113356178313.png"  # noqa: E501
+    expected = "https://nsm09.casimages.com/img/2021/03/17//21031703204821371617320516.png"  # noqa: E501
     assert res == expected
 
 
 def test_get_share(casim_inst):
     """Test Casim.get_share()."""
+    casim_inst.change_folder("Scantrad")
     res = casim_inst.get_share_code(4)
-    expected = ('[URL=https://www.casimages.com/i/200217113356178313.png.html]'  # noqa: E501
-                '[IMG]https://nsa40.casimages.com/img/2020/02/17/mini_200217113356178313.png[/IMG][/url]')  # noqa: E501
+    expected = ('[URL=https://www.casimages.com/i/21031703204821371617320516.png.html]'  # noqa: E501
+                '[IMG]https://nsm09.casimages.com/img/2021/03/17//mini_21031703204821371617320516.png[/IMG][/url]')  # noqa: E501
     assert res == expected
